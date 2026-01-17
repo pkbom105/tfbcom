01093289ff936abd5ef90b7208f3eb2c107bc513
Pannavith1 <pannavith1@tfb.local>
2026-01-17 12:10:35 +0700

    v1.1

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',      // ดึงไฟล์ออกมาเป็น Static HTML
-  
-  // 1. ใส่ชื่อโฟลเดอร์ที่คุณอัปโหลดขึ้นไป (เช่น pannavith.com/tfb/)
-  // ถ้าคุณอัปโหลดไว้ที่ Root (หน้าแรกสุด) ให้ลบบรรทัด basePath นี้ออก
-  basePath: '/tfb',  
-  assetPrefix: '/tfb', // เพิ่มบรรทัดนี้ครับ
-
-  // 2. ปิดระบบบีบอัดรูปภาพของ Next.js เพราะการทำ Static Export ไม่รองรับฟีเจอร์นี้บน Server ทั่วไป
-  images: {
-    unoptimized: true,
-  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
