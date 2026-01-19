6669b63eba21d0ba5030f40ca168f3ab49d7bae5
Pannavith1 <pannavith1@tfb.local>
2026-01-19 15:49:44 +0700

    version 1.6

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     return (
         <div className="text-xl text-center mt-4" >
             หน้าเเรก  
-            v1.5         
+            v1.6         
         </div>
     
     )  ;
