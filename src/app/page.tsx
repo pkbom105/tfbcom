54da2eb9af1c27f36de7aa2acfcd5a6745ef441a
Pannavith1 <pannavith1@tfb.local>
2026-01-19 11:16:14 +0700

    v1.3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Navbar } from "@/components/Navbar";
 
 export default function Home() {
   return (
-      <div>
+      <div className="text-xl text-center" >
           หน้าเเรก  
           v1.2         
     </div>
