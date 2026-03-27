572d6a524f4e08340e299ab92f192cdbb7226502
Pannavith1 <pannavith1@tfb.local>
2026-03-27 13:05:00 +0700

    version 2.75

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,11 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   images: {
-    domains: ['images.unsplash.com', 'www.pngmart.com'],
+    domains: ['images.unsplash.com', 'www.pngmart.com'], 
+    unoptimized: true, // จำเป็น: เพราะ FTP/Static Hosting ไม่มี Node.js มาจัดการย่อรูปให้
   },
+  output: 'export', // สำคัญมาก: เพื่อให้สร้างโฟลเดอร์ 'out' สำหรับ FTP
+  trailingSlash: true, // แนะนำ: ช่วยให้โครงสร้างโฟลเดอร์บน FTP เข้าใจง่ายขึ้น (เช่น /about/index.html) 
 };
 
 export default nextConfig;
